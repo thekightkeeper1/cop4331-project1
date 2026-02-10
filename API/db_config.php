1da5ad9b1968fde01b73ac77dfc2ae1069f40b0c
thekightkeeper1 <ty.singh828@gmail.com>
2026-02-10 16:00:03 -0500

    Update mysql user

--- API/db_config.php
+++ API/db_config.php
@@ -1,9 +1,9 @@
 <?php
 // db_config.php
 $host = "localhost";
-$dbname = "project1";
-$username = "kight";
-$password = "admin123";
+$dbname = "COP4331";
+$username = "karel";
+$password = "group9";
 
 # PHP Data Object for the database
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
