b02f6a9efb1a688429c6ee9905c32479ba546cfc
Jake-2-3 <jacobwsmith.203@gmail.com>
2026-02-13 18:10:01 -0500

    del

--- API/Contacts/Delete.php
+++ API/Contacts/Delete.php
@@ -1,3 +1,82 @@
 <?php
+<<<<<<< HEAD
 echo "hi";
 require_once "../db_config.php";
+=======
+
+	require_once '../db_config.php';
+
+    # Get the post request body
+	$inData = getRequestInfo();
+
+    # Variables for the register details
+	$newId = 0;
+
+	// # Checking that the user does not exist
+	$sql = "SELECT id FROM users WHERE id = :ids";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([
+		'ids' => $inData['id'],
+	]);
+	if ($stmt->fetch(PDO::FETCH_ASSOC)) {
+		returnWithError("Username already exists");
+		exit();
+	}
+
+	# Preparing the query with placeholders
+	$sql = "DELETE user FROM users WHERE id = :ids";
+	$stmt = $pdo->prepare($sql);
+
+	# Running the query and populating placeLholders
+	$worked = $stmt->execute([
+		'ids' => $inData["id"],
+	]);
+
+	if ($worked) {
+		returnWithInfo("Person removed");
+	} else
+	{
+		returnWithError("Unknown error occurred.");
+	}
+
+	# Closing the cursor we used. Not necessary unless we didn't read all of the rows.
+	// $stmt->closeCursor(); #todo remove this?
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+
+
+	function returnWithError($err)
+{
+    // 1. Create an associative array
+    $retValue = [
+        "id"    => 0,
+        "error" => $err
+    ];
+
+    // 2. Convert the array to a JSON string
+    $jsonResponse = json_encode($retValue);
+
+    sendResultInfoAsJson($jsonResponse);
+}
+	
+	function returnWithInfo($id)
+	{
+		$retValue = [
+			"id" => $id,
+			"error" => "",
+		];
+		sendResultInfoAsJson( json_encode($retValue) );
+	}
+	
+?>
+>>>>>>> db5e2b0 (del)
